fe6ef0549ff1ddb2d5131223b4a2628890dd869e
N Muhammed Nishan <98246947+nishankode@users.noreply.github.com>
2024-10-13 12:56:48 +0530

    Update youtube_scraper.py

--- youtube_scraper.py
+++ youtube_scraper.py
@@ -73,7 +73,7 @@ def get_recent_videos_for_handles(handles, hours=24):
 def get_video_transcript(video_id):
     """Retrieve the transcript for a specific video ID."""
     try:
-        video_transcript_json = YouTubeTranscriptApi.get_transcript(video_id, {'https': 'http://151.236.14.178:18080'})
+        video_transcript_json = YouTubeTranscriptApi.get_transcript(video_id, {'https': '51.159.159.73:80'})
         return ' '.join([i['text'] for i in video_transcript_json])
     except Exception as e:
         logging.error(f"Failed to retrieve transcript for {video_id}: {str(e)}")
